5a230fecc1f6d518baf04b44f379f880bd4c6556
Lars Tingelstad <lars.tingelstad@ntnu.no>
2020-09-21 11:37:32 +0000

    Added project structure

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # eris
 Hand-eye calibration from point clouds
+
+## Installation
+
+### Install dependencies
+```bash
+# GCC
+sudo apt-get install g++
+# CMake
+sudo apt-get install cmake cmake-curses-gui
+# google-glog + gflags
+sudo apt-get install libgoogle-glog-dev
+# BLAS & LAPACK
+sudo apt-get install libatlas-base-dev
+# Eigen3
+sudo apt-get install libeigen3-dev
+# Ninja
+sudo apt-get install ninja-build
+```
+
+### Download and install Ceres
+```bash
+wget http://ceres-solver.org/ceres-solver-1.14.0.tar.gz
+tar -xf ceres-solver-1.14.0.tar.gz 
+mkdir -p ceres-solver-1.14.0/build
+cd ceres-solver-1.14.0/build
+cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF -DBUILD_EXAMPLES=OFF ..
+cmake --build .
+cmake --build . --target install 
+```
---
+++ setup.py
@@ -0,0 +1,47 @@
+# Copyright 2019 Norwegian University of Science and Technology.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from skbuild import setup
+
+
+def main():
+    setup(
+        name="eris",
+        version="0.0.0",
+        description="Hand-eye calibration",
+        long_description=open("README.md").read(),
+        long_description_content_type="text/markdown",
+        url="",
+        author="Lars Tingelstad",
+        author_email="Lars Tingelstad@ntnu.no",
+        license="Apache License, Version 2.0",
+        packages=["eris", "_eris"],
+        package_dir={"": "modules"},
+        cmake_args=[
+            "-DCMAKE_BUILD_TYPE=Debug",
+            "-DCMAKE_INSTALL_RPATH=$ORIGIN",
+            "-DCMAKE_BUILD_WITH_INSTALL_RPATH:BOOL=ON",
+            "-DCMAKE_INSTALL_RPATH_USE_LINK_PATH:BOOL=OFF",
+        ],
+        classifiers=[
+            "License :: OSI Approved :: OSI Approved :: Apache Software License",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: Implementation :: CPython",
+            "Topic :: Scientific/Engineering",
+        ],
+    )
+
+
+if __name__ == "__main__":
+    main()
